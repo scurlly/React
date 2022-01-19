cd5ec280d2cf311143ae0e913a2245e6c3daa6f2
scurlly <61646477+scurlly@users.noreply.github.com>
2022-01-19 11:38:40 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h1>Kpop Groups</h1>
+    <ul>
+      <li>BTS</li>
+      <li>Stray Kids</li>
+      <li>Ateez</li>
+      <li>Itzy</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
