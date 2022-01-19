53699b4172ceac09bd95f56636bf3770ebe14e2c
scurlly <61646477+scurlly@users.noreply.github.com>
2022-01-19 13:26:43 +0530

    Js expressions practice

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from "react-dom";
 
 const fname = "Scurlly";
 const lname = "Rodrigues";
-const num = "17";
+const date = new Date();
+const year = date.getFullYear();
 
 ReactDOM.render(
   <div>
-    <h1>My name is {fname + " " + lname}!</h1>
-    <ul>
-      <li>BTS</li>
-      <li>Stray Kids</li>
-      <li>Ateez</li>
-      <li>Itzy</li>
-    </ul>
-    <p>My favourite number is {Math.floor(Math.random() * 10)}</p>
+    <p>
+      Created by {fname} {lname}
+    </p>
+    <p>Copyright {year}</p>
   </div>,
   document.getElementById("root")
 );
