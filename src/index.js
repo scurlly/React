c18c32e9c17a3e56213c43f143ee0488cafca42d
scurlly <61646477+scurlly@users.noreply.github.com>
2022-01-19 13:11:41 +0530

    expressions in js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fname = "Scurlly";
+const lname = "Rodrigues";
+const num = "17";
+
 ReactDOM.render(
   <div>
-    <h1>Kpop Groups</h1>
+    <h1>My name is {fname + " " + lname}!</h1>
     <ul>
       <li>BTS</li>
       <li>Stray Kids</li>
       <li>Ateez</li>
       <li>Itzy</li>
     </ul>
+    <p>My favourite number is {Math.floor(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root")
 );
